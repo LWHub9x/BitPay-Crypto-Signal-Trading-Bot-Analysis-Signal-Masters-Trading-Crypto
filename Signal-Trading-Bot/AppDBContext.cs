25aa6cc4a87c2c8844e9bdb1fbf0bf0e623f7426
OyubJun <c567tiuwertcx@outlook.com>
2024-08-09 14:14:59 +0400

    Upload Files

---
+++ Signal-Trading-Bot/AppDBContext.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using RealTimeSQL.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RealTimeSQL.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext()
+        {
+
+        }
+        public AppDBContext(DbContextOptions<AppDBContext> options)
+          : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //optionsBuilder.UseMySql(Constr, new MySqlServerVersion(new Version(8, 0, 21)));
+            }
+        }
+        public DbSet<TBL_Coin> TBL_Coin { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
