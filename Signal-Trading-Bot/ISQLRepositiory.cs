a107195adeec5ce88529543b0269bfa7e74d67a3
OyubJun <c567tiuwertcx@outlook.com>
2024-08-09 14:15:11 +0400

    Upload Files

---
+++ Signal-Trading-Bot/ISQLRepositiory.cs
@@ -0,0 +1,11 @@
+﻿
+using RealTimeSQL.Models;
+using System.Collections.Generic;
+
+namespace RealTimeSQL.Repository
+{
+    public interface ISQLRepositiory
+    {
+        List<TBL_Coin> GetAllCoin();
+    }
+}
