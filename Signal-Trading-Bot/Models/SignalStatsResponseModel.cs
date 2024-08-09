15eba84ce08e1ab2edd2fad1489badac8ac3ebcb
OyubJun <c567tiuwertcx@outlook.com>
2024-08-09 14:16:10 +0400

    Upload Files

---
+++ Signal-Trading-Bot/Models/SignalStatsResponseModel.cs
@@ -0,0 +1,11 @@
+﻿using CryptoSignalChecker.Enums;
+
+namespace CryptoSignalChecker.Models
+{
+    public class SignalStatsResponseModel
+    {
+        public DateTime StopLoss { get; set; }
+        public DateTime AprxmtLiquidation { get; set; }
+        public List<TakeProfitStats> TakeProfitStats { get; set; } = new List<TakeProfitStats>();
+    }
+}
