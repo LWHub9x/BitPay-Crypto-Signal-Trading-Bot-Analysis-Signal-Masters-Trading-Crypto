745e6893581f43f6b407d078ee858d8d0e1db3ff
OyubJun <c567tiuwertcx@outlook.com>
2024-08-09 14:15:16 +0400

    Upload Files

---
+++ Signal-Trading-Bot/SQLHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealTimeSQL.Hubs
+{
+    public class SQLHub : Hub
+    {
+
+    }
+
+}
