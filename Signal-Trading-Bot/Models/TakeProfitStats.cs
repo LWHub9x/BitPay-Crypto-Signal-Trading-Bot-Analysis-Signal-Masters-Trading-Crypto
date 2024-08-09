8409ee33dc5da08bf315bca28c83c88d88261467
OyubJun <c567tiuwertcx@outlook.com>
2024-08-09 14:16:12 +0400

    Upload Files

---
+++ Signal-Trading-Bot/Models/TakeProfitStats.cs
@@ -0,0 +1,12 @@
+﻿namespace CryptoSignalChecker.Models
+{
+    public class TakeProfitStats
+    {
+        public double Value { get; set; }
+        public DateTime DateTime { get; set; }
+        public TakeProfitStats(double value)
+        {
+            Value = value;
+        }
+    }
+}
