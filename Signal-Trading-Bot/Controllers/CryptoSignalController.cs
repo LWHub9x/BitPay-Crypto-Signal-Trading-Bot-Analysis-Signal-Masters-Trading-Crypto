9742bf2c5a60a9256299e0a87f2e0e592537befa
OyubJun <c567tiuwertcx@outlook.com>
2024-08-09 14:15:21 +0400

    Upload Files

---
+++ Signal-Trading-Bot/Controllers/CryptoSignalController.cs
@@ -0,0 +1,25 @@
+﻿using CryptoSignalChecker.Interfaces;
+using CryptoSignalChecker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoSignalChecker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CryptoSignalController : ControllerBase
+    {
+        private readonly ICryptoSignalService _cryptoSignalService;
+
+        public CryptoSignalController(ICryptoSignalService cryptoSignalService)
+        {
+            _cryptoSignalService = cryptoSignalService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetTokenRate(SignalStatsRequestModel request)
+        {
+            var result = await _cryptoSignalService.GetRate(request);
+            return Ok(result);
+        }
+    }
+}
