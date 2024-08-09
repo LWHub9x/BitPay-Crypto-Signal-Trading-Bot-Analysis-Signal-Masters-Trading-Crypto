11e63a4b9fc1ecfc02903c4fcd0d5c09ea539378
OyubJun <c567tiuwertcx@outlook.com>
2024-08-09 14:15:45 +0400

    Upload Files

---
+++ Signal-Trading-Bot/HttpClients/Models/TokenRate.cs
@@ -0,0 +1,28 @@
+﻿namespace CryptoSignalChecker.HttpClients.Models
+{
+    public class TokenRate
+    {
+        public long KlineOpenTime { get; set; }
+        public double OpenPrice { get; set; }
+        public double HighPrice { get; set; }
+        public double LowPrice { get; set; }
+        public double ClosePrice { get; set; }
+        public long KlineCloseTime { get; set; }
+
+        public TokenRate(
+            long klineOpenTime,
+            double openPrice,
+            double highPrice,
+            double lowPrice,
+            double closePrice,
+            long klineCloseTime)
+        {
+            KlineOpenTime = klineOpenTime;
+            OpenPrice = openPrice;
+            HighPrice = highPrice;
+            LowPrice = lowPrice;
+            ClosePrice = closePrice;
+            KlineCloseTime = klineCloseTime;
+        }
+    }
+}
