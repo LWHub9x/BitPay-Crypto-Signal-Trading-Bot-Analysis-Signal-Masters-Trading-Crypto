2d48d44b83f48ba7d52210cee6c2e117b4fa1cc5
OyubJun <c567tiuwertcx@outlook.com>
2024-08-09 14:15:19 +0400

    Upload Files

---
+++ Signal-Trading-Bot/TBL_Coin.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealTimeSQL.Models
+{
+    public class TBL_Coin
+    {
+        public string id { get; set; }
+        public string slug { get; set; }
+        public string symbol { get; set; }
+        [Display(Name = "Price [USD]")]
+        public decimal? price_usd { get; set; }
+        public string timestamp { get; set; }
+    }
+}
